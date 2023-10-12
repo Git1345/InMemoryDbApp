1d970b8383fa273b34ba49e76707cd3438066dd0
shubham <shubham.choudhary@bellurbis.com>
2023-10-12 18:10:54 +0530

    Add project files.

---
+++ InMemoryDbApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace InMemoryDbApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
